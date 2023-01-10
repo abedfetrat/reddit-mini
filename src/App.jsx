cf296c832bf37d0ad5d7e33de8b98e750580589b
Abed Fetrat <abedfetrat@gmail.com>
2023-01-10 21:27:55 +0100

    setup mui theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-import { CssBaseline } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff4500",
+    },
+    secondary: {
+      main: "#448aff",
+    },
+  },
+});
 
 function App() {
   return (
     <>
       <CssBaseline />
+      <ThemeProvider theme={theme}></ThemeProvider>
     </>
   );
 }
