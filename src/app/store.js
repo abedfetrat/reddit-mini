e1e15eaed4d074be3090d0e87e67c73f7fdc063d
Abed Fetrat <abedfetrat@gmail.com>
2023-01-20 12:55:49 +0100

    add posts slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import favoriteSubredditsReducer from "./favoriteSubredditsSlice";
 import searchTermReducer from "./searchTermSlice";
+import postsReducer from "./postsSlice";
 
 const store = configureStore({
     reducer: {
         favoriteSubreddits: favoriteSubredditsReducer,
         searchTerm: searchTermReducer,
+        posts: postsReducer,
     }
 });
 
